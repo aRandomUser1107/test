2644e4e947ced7479b315764d1995ba93b18fddb
aRandomUser1107 <yvela.cline@gmail.com>
2025-05-16 10:38:46 +0700

    update

---
+++ src/sclassdev/app_todo/views/create.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app_todo.utility import query
+
+def view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        result = query("INSERT INTO todo_post (title, content) VALUES (%s, %s)", [title, content])
+        print(result)
+
+    return render(request, 'app_todo/create.html')
